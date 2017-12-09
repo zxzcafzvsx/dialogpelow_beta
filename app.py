9c5b570ebd9176cd09333fb50dbc4e8feeb15108
zxzcafzvsx <31308588+zxzcafzvsx@users.noreply.github.com>
2017-12-10 05:31:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def processRequest(req):
         yql_query = makeYqlQueryForShalat(req)
         if yql_query is None:
            return {}
-        yql_url = baseurl + urlencode({yql_query})
+        yql_url = baseurl + yql_query
         result = urlopen(yql_url).read()
         data = json.loads(result)
         res = makeWebhookResultForShalat(data)
